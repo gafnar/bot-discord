bf456112686289db230eabce9b465803a747a81f
Jorge del Portillo <gafnar@gmail.com>
2019-09-05 21:40:13 +0200

    Delete diablo.jpg (#2)

--- backend/src/config/audio.js
+++ backend/src/config/audio.js
@@ -3,7 +3,6 @@ module.exports = {
   seh: `${path}/seeh.mp3`,
   scream: `${path}/grito.mp3`,
   rol: `${path}/rol.mp3`,
-  vgatos: `${path}/vgatos.mp3`,
   amai: `${path}/amai.mp3`,
   gallina: `${path}/gallina.mp3`,
 };
