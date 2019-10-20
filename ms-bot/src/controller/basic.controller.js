2d23466dd45ed9fb01dd4353f77b23f0ab09b23e
Jorge <jorge.rodriguez@yudonpay.com>
2019-10-20 20:47:04 +0200

    Clean method clear

--- ms-bot/src/controller/basic.controller.js
+++ ms-bot/src/controller/basic.controller.js
@@ -1,18 +1,23 @@
 const config = require('../config/config');
 const botError = require('../service/bot.error.service');
 const { basic } = require('../responses/bot.response');
-const { clearUndefinedNumber } = basic;
+const { clearUndefinedNumber, clearLess, clearMore } = basic;
+
+const getDeleteCountToArgs = (number) => {
+  if(isNaN(number)) throw new botError(clearUndefinedNumber);
+  const deleteCount = parseInt(number, 10);
+  if(deleteCount < 2 ) throw new botError(clearLess);
+  if(deleteCount > 100) throw new botError(clearMore);
+  return deleteCount;
+}
+
 module.exports = {
   ping: async (message, bot) => {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
   },
   clear: async (message, bot, args) => {
-    if(Number(args[0]) === 'NaN') throw new botError(clearUndefinedNumber);
-    const deleteCount = parseInt(args[0], 10);
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100) {
-      return message.reply("Dime cuantos mensajes borro");
-    }
+    const deleteCount = getDeleteCountToArgs(args[0]);
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     await message.channel.bulkDelete(fetched)
   }
--- ms-bot/src/responses/bot.response.js
+++ ms-bot/src/responses/bot.response.js
@@ -5,5 +5,7 @@ module.exports = {
   },
   basic: {
     clearUndefinedNumber: res(400, 400, 'Error con el numero de mensajes',[{ msg: 'Error con el numero de mensajes' }]),
+    clearLess: res(400, 400, `No se puede borrar menos de 2 mensajes`,[{ msg: `No se puede borrar menos de 2 mensajes` }]),
+    clearMore: res(400, 400, `No se puede borrar más de 100 mensajes`,[{ msg: `No se puede borrar más de 100 mensajes` }]),
   },
 };
